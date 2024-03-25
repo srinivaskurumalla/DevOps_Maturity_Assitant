31091af755fc34dbc4fb734fcbb97e7647b08501
srinivaskurumalla <kurumallasrinivas123@gmail.com>
2024-03-25 14:01:25 +0530

    default path updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'feature', loadChildren: () => import('./components/feature1/feature1.module').then(m => m.Feature1Module) }
+  { path: 'feature', loadChildren: () => import('./components/feature1/feature1.module').then(m => m.Feature1Module) },
+  { path: '**', loadChildren: () => import('./components/feature1/feature1.module').then(m => m.Feature1Module) },
 ];
 
 
